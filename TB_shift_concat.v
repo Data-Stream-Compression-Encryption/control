769ca2f935cad84005842b204e6a4076e79a1c7a
alex <ahc24@vt.edu>
2015-02-10 14:36:04 -0500

    shift_concat module complete and "bug free" rigorous testing needed

--- TB_shift_concat.v
+++ TB_shift_concat.v
@@ -41,30 +41,53 @@ initial begin
   rst = 0;
   #4 rst = 1;
   #8 rst = 0;
-  
-	test_value = 64'h000abcdef1234567;
-	valid_bits = 7'd52;	
-	msg_fin = 1'b0;
-	#10 data_valid = 1'b1;
-	#10 data_valid = 1'b0;
+ 
+  while(1) begin
+	 test_value = 64'h000abcdef1234567;
+	 valid_bits = 7'd52;	
+	 msg_fin = 1'b0;
+	 #10 data_valid = 1'b1;
+	 #10 data_valid = 1'b0;
 	
-	test_value = 64'h000000000000000e;
-	valid_bits = 7'd4;	
-	msg_fin = 1'b0;
-	#10 data_valid = 1'b1;
-	#10 data_valid = 1'b0;
+	 test_value = 64'h000000000000000e;
+	 valid_bits = 7'd4;	
+	 msg_fin = 1'b0;
+	 #10 data_valid = 1'b1;
+	 #10 data_valid = 1'b0;
 	
-  test_value = 64'h000000000000ebf3;
-	valid_bits = 7'd16;	
-	msg_fin = 1'b0;
-	#10 data_valid = 1'b1;
-	#10 data_valid = 1'b0;
+   test_value = 64'h000000000000ebf3;
+	 valid_bits = 7'd16;	
+	 msg_fin = 1'b0;
+	 #10 data_valid = 1'b1;
+	 #10 data_valid = 1'b0;
 	
-	test_value = 64'h00000000000000ec;
-	valid_bits = 7'd8;	
-	msg_fin = 1'b0;
-	#10 data_valid = 1'b1;
-	#10 data_valid = 1'b0;
+	 test_value = 64'h00000000000000ec;
+	 valid_bits = 7'd8;	
+	 msg_fin = 1'b0;
+	 #10 data_valid = 1'b1;
+	 #10 data_valid = 1'b0;
+	
+	 test_value = 64'hfedcba9876543210;
+	 valid_bits = 7'd64;	
+	 msg_fin = 1'b1;
+	 #10 data_valid = 1'b1;
+	 #10 data_valid = 1'b0;
+	 msg_fin = 0'b1;
+	
+	 test_value = 64'hfedcba9876543210;
+	 valid_bits = 7'd0;	
+	 msg_fin = 1'b0;
+	 #10 data_valid = 1'b1;
+	 #10 data_valid = 1'b0; 
+	 
+	 test_value = 64'hfedcba9876543210;
+	 valid_bits = 7'd0;	
+	 msg_fin = 1'b0;
+	 #10 data_valid = 1'b1;
+	 #10 data_valid = 1'b0; 
+	 
+	end
+	
 end
 	
 
--- shift_concat.v
+++ shift_concat.v
@@ -14,12 +14,14 @@ module shift_concat(clk, rst, data_in, data_valid, valid_bits, msg_fin, data_out
   input data_valid;           // Indicates that the data on data_in is valid
   input msg_fin;              // Indicates for the module to output remaining data regardless of completeness 
   
-  output done;
-  output [63:0] data_out;
+  output done;                // Indicates to subsequent modules that the output is valid
+  output [63:0] data_out;     // Data output
   
   reg [127:0] concat_reg;     // Register for "stacking" input segments
   reg [7:0] concat_reg_valid; // Indicates the number of valid bits in the least significant 64 bits ot the concat_reg            
   reg msg_fin_reg;
+  
+  wire [63:0] valid_mask;
   
   // ==============================
   // Output drivers
@@ -29,6 +31,8 @@ module shift_concat(clk, rst, data_in, data_valid, valid_bits, msg_fin, data_out
   assign done = ( concat_reg_valid >= 64 )? 1 : 
                   (msg_fin_reg)? 1 : 
                   0 ;
+                  
+  assign valid_mask = 64'hffffffffffffffff >> (64 - valid_bits);
   
   // ==============================
   // register drivers
@@ -39,11 +43,11 @@ module shift_concat(clk, rst, data_in, data_valid, valid_bits, msg_fin, data_out
     if( rst )
       concat_reg <= 0;     
     else if( (data_valid) && (concat_reg_valid >= 64) )
-      concat_reg <= (data_in << concat_reg_valid - 64) | (concat_reg >> 64);
+      concat_reg <= ((data_in & valid_mask) << concat_reg_valid - 64) | (concat_reg >> 64);
     else if(concat_reg_valid >= 64)
       concat_reg <= concat_reg >> 64;
-    else if(data_valid)  
-      concat_reg <= (concat_reg)|(data_in << concat_reg_valid);
+    else if(data_valid && (valid_bits != 64'b0))  
+      concat_reg <= (concat_reg)|((data_in & valid_mask) << concat_reg_valid);
     else if (msg_fin_reg)
       concat_reg <= 0; 
     else
@@ -59,7 +63,7 @@ module shift_concat(clk, rst, data_in, data_valid, valid_bits, msg_fin, data_out
         concat_reg_valid <= concat_reg_valid + valid_bits - 64;
       else if(concat_reg_valid >= 64)
         concat_reg_valid <= concat_reg_valid - 64;
-      else if(data_valid)
+      else if(data_valid && (valid_bits != 64'b0) )
         concat_reg_valid <= concat_reg_valid + valid_bits;
       else if(msg_fin_reg)
         concat_reg_valid <= 0; 
